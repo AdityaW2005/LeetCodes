0746e72f05f8124f67145e45e975941b3062a610
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-08-07 16:32:20 +0530

    Time: 43 ms (31.53%) | Memory: 18 MB (16.97%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,20 +3,14 @@ class Solution:
         revNum = 0
         intMax = 2**31 - 1
         intMin = -2**31
-
         isNegative = x < 0
         x = abs(x)
-
         while x > 0:
             lastDigit = x % 10
             x = x // 10
             revNum = revNum * 10 + lastDigit
-
         if isNegative:
             revNum = -revNum
-
-        # Check overflow after reversing
         if revNum < intMin or revNum > intMax:
             return 0
-
         return revNum
