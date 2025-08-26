ddd3a9fa91cb676d107598d4a12b60d8b589001c
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-08-26 22:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.13%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,8 +1,23 @@
+# Brute Force Method
+# class Solution:
+#     def check(self, nums: List[int]) -> bool:
+#         for i in range(len(nums)):
+#             a = nums.pop(0)
+#             nums.append(a)
+#             if nums == sorted(nums):
+#                 return True
+#         return False
+
+# Optimized Solution
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            a = nums.pop(0)
-            nums.append(a)
-            if nums == sorted(nums):
+        N = len(nums)
+        count = 1
+        for i in range(1, 2 * N):
+            if nums[(i - 1) % N] <= nums[i % N]:
+                count += 1
+            else:
+                count = 1
+            if count == N:
                 return True
-        return False
+        return N == 1
