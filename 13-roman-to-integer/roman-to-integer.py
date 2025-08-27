e6271fc2a2759bd31f4bae79ac42e26a9868f2b7
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-08-27 23:24:49 +0530

    Time: 7 ms (39.80%) | Memory: 18 MB (20.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -3,7 +3,6 @@ class Solution:
         romanSymbols = {'I':1, 'V':5, 'X':10, 'L':50,  'C':100, 'D':500, 'M':1000}
         total = 0
         prev_value = 0
-
         for char in reversed(s):
             value = romanSymbols[char]
             if value >= prev_value:
