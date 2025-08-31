b0e4b47ec454e13afeb51c12eeb0205dbbc4ec91
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-08-31 11:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.85%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,31 +1,21 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        temp = ListNode()
-        head = temp
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        curr = dummy
+        
         while list1 and list2:
             if list1.val <= list2.val:
-                if head == None:
-                    head = list1
-                else:
-                    head.next = list1
-                    list1 = list1.next
+                curr.next = list1
+                list1 = list1.next
             else:
-                if head == None:
-                    head = list2
-                else:
-                    head.next = list2
-                    list2 = list2.next
-            head = head.next
-        if list1:
-            head.next = list1
-            list1 = list1.next
-        if list2:
-            head.next = list2
-            list2 = list2.next
-        return temp.next
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
         
+        curr.next = list1 if list1 else list2
+        return dummy.next
