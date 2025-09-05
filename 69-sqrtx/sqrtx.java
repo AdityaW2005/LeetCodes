38e3195415b956e56a8ed06870c708d8d817c6f4
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-05 20:50:12 +0530

    Time: 1 ms (84.22%) | Memory: 40.9 MB (69.74%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,20 +1,33 @@
+// public class Solution {
+//     public int mySqrt(int x) {
+//         if (x < 2) return x;
+
+//         int left = 1, right = x / 2 + 1; // sqrt(x) <= x/2+1 for x>=2
+//         int ans = 1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (mid <= x / mid) {   // mid*mid <= x without overflow
+//                 ans = mid;          // mid is feasible
+//                 left = mid + 1;     // try bigger
+//             } else {
+//                 right = mid - 1;    // too big
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+//Newton's Method
 public class Solution {
     public int mySqrt(int x) {
         if (x < 2) return x;
 
-        int left = 1, right = x / 2 + 1; // sqrt(x) <= x/2+1 for x>=2
-        int ans = 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (mid <= x / mid) {   // mid*mid <= x without overflow
-                ans = mid;          // mid is feasible
-                left = mid + 1;     // try bigger
-            } else {
-                right = mid - 1;    // too big
-            }
+        long y = x; // use long to avoid overflow in y*y
+        while (y * y > x) {
+            y = (y + x / y) / 2;
         }
-        return ans;
+        return (int) y;
     }
 }
