6dc55a33120dceb82f64051a7cbe71b9eeee6383
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-08 12:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.69%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,31 +1,32 @@
-# class Solution:
-#     def getNoZeroIntegers(self, n: int) -> List[int]:
-#         def has_zero(num: int) -> bool:
-#             return '0' in str(num)
-#         for a in range(1, n):
-#             b = n - a
-#             if not has_zero(a) and not has_zero(b):
-#                 return [a, b]
-
-
+# Optimized Code
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        a = 0
-        b = 0
-        power_of_10 = 1
-        
-        while n > 0:
-            digit = n % 10
-            n = n // 10
+        def has_zero(num: int) -> bool:
+            return '0' in str(num)
+        for a in range(1, n):
+            b = n - a
+            if not has_zero(a) and not has_zero(b):
+                return [a, b]
 
-            if (digit == 0 or digit == 1) and n > 0:
-                a += 2 * power_of_10
-                b += (digit + 8) * power_of_10
-                n -= 1
-            else:
-                a += 1 * power_of_10
-                b += (digit - 1) * power_of_10
+# My Logic
+# class Solution:
+#     def getNoZeroIntegers(self, n: int) -> List[int]:
+#         a = 0
+#         b = 0
+#         power_of_10 = 1
+        
+#         while n > 0:
+#             digit = n % 10
+#             n = n // 10
+
+#             if (digit == 0 or digit == 1) and n > 0:
+#                 a += 2 * power_of_10
+#                 b += (digit + 8) * power_of_10
+#                 n -= 1
+#             else:
+#                 a += 1 * power_of_10
+#                 b += (digit - 1) * power_of_10
             
-            power_of_10 *= 10
+#             power_of_10 *= 10
             
-        return [a, b]
+#         return [a, b]
