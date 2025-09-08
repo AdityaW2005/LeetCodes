4baba5345c95bf13384f36d1545a5734b5a770b9
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-08 12:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.73%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,8 +1,32 @@
+# class Solution:
+#     def getNoZeroIntegers(self, n: int) -> List[int]:
+#         def has_zero(num: int) -> bool:
+#             return '0' in str(num)
+#         for a in range(1, n):
+#             b = n - a
+#             if not has_zero(a) and not has_zero(b):
+#                 return [a, b]
+
+from typing import List
+
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        def has_zero(num: int) -> bool:
-            return '0' in str(num)
-        for a in range(1, n):
-            b = n - a
-            if not has_zero(a) and not has_zero(b):
-                return [a, b]
+        a = 0
+        b = 0
+        power_of_10 = 1
+        
+        while n > 0:
+            digit = n % 10
+            n = n // 10
+
+            if (digit == 0 or digit == 1) and n > 0:
+                a += 2 * power_of_10
+                b += (digit + 8) * power_of_10
+                n -= 1
+            else:
+                a += 1 * power_of_10
+                b += (digit - 1) * power_of_10
+            
+            power_of_10 *= 10
+            
+        return [a, b]
