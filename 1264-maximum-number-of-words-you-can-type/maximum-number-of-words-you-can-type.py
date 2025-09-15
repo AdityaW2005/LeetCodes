35241998ff5725415e0dec4ff3ef68a1e76a8482
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-15 23:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.48%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,25 +1,10 @@
-# Brute Force Code
-
-# class Solution:
-#     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-#         brokenLettersList = []
-#         splitText = text.split()
-#         count = 0
-#         for i in brokenLetters:
-#             brokenLettersList.append(i)
-#         for word in splitText:
-#             isBroken = False
-#             for b in brokenLettersList:
-#                 if b in word:
-#                     isBroken = True
-#                     break
-#             if not isBroken:
-#                 count += 1
-#         return count
-
-# Optimized Code
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        broken = set(brokenLetters)
-        return sum(all(c not in broken for c in word) for word in text.split())
-
+        count = 0
+        for word in text.split():
+            for letter in word:
+                if letter in brokenLetters:
+                    count += 1
+                    break
+        return len(text.split()) - count
+        
