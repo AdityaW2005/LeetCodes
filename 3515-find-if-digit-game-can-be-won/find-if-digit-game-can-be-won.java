f3989acf473de9ce7b5c85f412a6b473688ea3c7
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-22 16:14:32 +0530

    Time: 2 ms (4.33%) | Memory: 44.4 MB (28.41%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -4,8 +4,10 @@ class Solution {
         int doubleDigitSum = 0;
         for (int i = 0; i < nums.length; i++){
             String s = Integer.toString(nums[i]);
-            if (s.length() % 2 == 1) singleDigitSum += nums[i];
-            else doubleDigitSum += nums[i];
+            if (s.length() % 2 == 1) 
+                singleDigitSum += nums[i];
+            else 
+                doubleDigitSum += nums[i];
         }
         if (singleDigitSum > doubleDigitSum || doubleDigitSum > singleDigitSum) 
             return true;
