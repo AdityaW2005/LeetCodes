25fcfe73a5082eec5388d290da94417a87324c7c
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-24 13:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.33%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,4 +1,20 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+        # Using Binary Search
+
+        # low = 1
+        # right = num
+
+        # while low <= right:
+        #     mid = (low+right)//2
+        #     res = mid * mid
+        #     if res == num:
+        #         return True
+        #     elif res < num:
+        #         low = mid+1
+        #     else:
+        #         right = mid-1
+        # return False
+
         a, b = math.modf(num ** 0.5)
         return a == 0
