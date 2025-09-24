942455ab16ab3356d9f282321d431843089fab54
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-25 00:14:30 +0530

    Time: 7 ms (89.20%) | Memory: 18.1 MB (34.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        count = 0
-        chars = list(s) 
-        for i in t:
-            if i in chars:
-                count += 1
-                chars.remove(i) 
-            else:
-                return False  
-        return count == len(t)
+        # if len(s) != len(t):
+        #     return False
+        # count = 0
+        # chars = list(s) 
+        # for i in t:
+        #     if i in chars:
+        #         count += 1
+        #         chars.remove(i) 
+        #     else:
+        #         return False  
+        # return count == len(t)
+
+        return Counter(s) == Counter(t)
