4a19c497601ac501fefabf2a9dbdce6c67dbf984
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-25 00:22:55 +0530

    Time: 6 ms (41.67%) | Memory: 43.3 MB (71.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,51 +2,51 @@ import java.util.HashMap;
 import java.util.Map;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if (s.length() != t.length()) {
-        //     return false;
-        // }
+        // Optimized Code
         
-        // int[] charCount = new int[26]; // For lowercase English letters
-        
-        // // Process both strings simultaneously
-        // for (int i = 0; i < s.length(); i++) {
-        //     charCount[s.charAt(i) - 'a']++;
-        //     charCount[t.charAt(i) - 'a']--;
-        // }
-        
-        // // Check if all counts are zero
-        // for (int count : charCount) {
-        //     if (count != 0) {
-        //         return false;
-        //     }
-        // }
-        
-        // return true;
-
         if (s.length() != t.length()) {
             return false;
         }
         
-        Map<Character, Integer> charCount = new HashMap<>();
+        int[] charCount = new int[26]; // For lowercase English letters
         
-        // Count characters in string s
-        for (char c : s.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        // Process both strings simultaneously
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
         }
         
-        // Decrement counts using string t
-        for (char c : t.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) - 1);
-            if (charCount.get(c) < 0) {
+        // Check if all counts are zero
+        for (int count : charCount) {
+            if (count != 0) {
                 return false;
             }
         }
         
         return true;
+
+
+        // Hashmap Approach
+
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        
+        // Map<Character, Integer> charCount = new HashMap<>();
+        
+        // // Count characters in string s
+        // for (char c : s.toCharArray()) {
+        //     charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        // }
+        
+        // // Decrement counts using string t
+        // for (char c : t.toCharArray()) {
+        //     charCount.put(c, charCount.getOrDefault(c, 0) - 1);
+        //     if (charCount.get(c) < 0) {
+        //         return false;
+        //     }
+        // }
+        
+        // return true;
     }
 }
-
-
-
-
-        
