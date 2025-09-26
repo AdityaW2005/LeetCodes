1b5cef59f22fda8c618d09cf2a1d9151d9d6ed03
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-26 13:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.30%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -6,27 +6,27 @@ class Solution:
             return 99
         if num == 996:
             return 999
+
         a = str(num)
         l = list(a)
+
         if num == 966 or num == 969:
             l[1] = '9'
-            s = int(''.join(map(str,l)))
-            return s
-        if num >= 9666 and num <= 9699:
-            l[1] = '9'
-            s = int(''.join(map(str,l)))
-            return s
-        if num == 9966 or num == 9969:
-            l[2] = '9'
-            s = int(''.join(map(str,l)))
-            return s
-        if num == 9996:
-            l[3] = '9'
-            s = int(''.join(map(str,l)))
-            return s
+            n = int(''.join(map(str,l)))
+            return n
         if num <= 6999:
             l[0] = '9'
-            s = int(''.join(map(str,l)))
-            return s
-        
-            
+            n = int(''.join(map(str,l)))
+            return n
+        if num >= 9666 and num <= 9699:
+            l[1] = '9'
+            n = int(''.join(map(str,l)))
+            return n
+        if num == 9966 or num == 9969:
+            l[2] = '9'
+            n = int(''.join(map(str,l)))
+            return n
+        if num == 9996:
+            l[3] = '9'
+            n = int(''.join(map(str,l)))
+            return n
