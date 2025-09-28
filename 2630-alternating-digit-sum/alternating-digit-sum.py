0476e4b2e292d2067bae0bfeb2bba9fc28d4a1bc
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-28 17:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.33%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.py
+++ 2630-alternating-digit-sum/alternating-digit-sum.py
@@ -1,11 +1,6 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        # ans = 0
-        # for i, c in enumerate(str(n)):
-        #     ans += int(c) * (-1) ** i
-        # return ans
-
-        alternateDigitSum = 0
+        ans = 0
         for i in range(len(str(n))):
-            alternateDigitSum += int(str(n)[i]) * (-1) ** i
-        return alternateDigitSum
+            ans += int(str(n)[i]) * (-1) ** i
+        return ans
