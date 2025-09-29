e958c9933a54aff2c2e6933016f126aeb0d10874
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-09-29 23:46:44 +0530

    Time: 1685 ms (34.67%) | Memory: 56.2 MB (95.63%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        
+        b = [False] * n  # Boolean array to mark non-primes
+        count = 0
+        
+        for i in range(2, int(n ** 0.5) + 1):
+            if not b[i]:
+                for j in range(i * i, n, i):
+                    b[j] = True
+        
+        for i in range(2, n):
+            if not b[i]:
+                count += 1
+        
+        return count
