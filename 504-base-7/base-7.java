068c7978098e29ed4b1bf3b022e61e4cd031bea2
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-06 19:45:58 +0530

    Time: 2 ms (20.58%) | Memory: 40.8 MB (93.57%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -4,12 +4,13 @@ class Solution {
 
         boolean neg = num < 0;     
         int n = Math.abs(num);     
-
         StringBuilder sb = new StringBuilder();
+        
         while (n > 0) {            
             sb.append(n % 7);     
             n /= 7;               
         }
+
         String res = sb.reverse().toString(); 
         return neg ? "-" + res : res;        
     }
