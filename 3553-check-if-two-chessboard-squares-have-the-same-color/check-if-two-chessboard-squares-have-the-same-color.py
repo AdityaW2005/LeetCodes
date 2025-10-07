fe565a98736a9529e49fc4d2ca363816e60cf250
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-07 23:39:50 +0530

    Time: 44 ms (21.29%) | Memory: 18 MB (25.72%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -2,15 +2,14 @@ class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         d = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
         l = []
-        if coordinate1[0] in d.keys():
-            if (d[coordinate1[0]] + int(coordinate1[-1])) % 2 == 0:
-                l.append(True)
-            else:
-                l.append(False)
-        if coordinate2[0] in d.keys():
-            if (d[coordinate2[0]] + int(coordinate2[-1])) % 2 == 0:
-                l.append(True)
-            else:
-                l.append(False)
+        if (coordinate1[0] in d.keys()) and (d[coordinate1[0]] + int(coordinate1[-1])) % 2 == 0:    
+            l.append(True)
+        else:
+            l.append(False)
+
+        if (coordinate2[0] in d.keys()) and (d[coordinate2[0]] + int(coordinate2[-1])) % 2 == 0:   
+            l.append(True)
+        else:
+            l.append(False)
         return l[0] == l[-1]
         
