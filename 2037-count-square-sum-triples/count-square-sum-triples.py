e49abc2f00ac4fe7fd916200b39870d88b7333bd
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-09 17:44:48 +0530

    Time: 87 ms (89.08%) | Memory: 17.6 MB (82.13%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -3,7 +3,8 @@ class Solution:
         squares = {x * x for x in range(1, n + 1)}
         count = 0
         for i in range(1, n + 1):
+            i2 = i * i
             for j in range(1, n + 1):
-                if i * i + j * j in squares:
+                if i2 + j * j in squares:
                     count += 1
         return count
