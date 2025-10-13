0af18d9a0a0cbfa089aa226016e7700cd45b4b95
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-13 22:32:41 +0530

    Time: 30 ms (92.02%) | Memory: 17.8 MB (30.74%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
@@ -1,10 +1,9 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        if (high - low + 1) % 2 == 1 and (high % 2 == 1 and low % 2 == 1):
-            return math.ceil((high - low + 1) / 2)
-        elif (high - low + 1) % 2 == 1 and (high % 2 != 1 and low % 2 != 1):
-            return (high - low + 1) // 2
-        elif (high - low + 1) % 2 == 0 and ((high % 2 == 1 and low % 2 == 0) or (high % 2 == 0 and low % 2 == 1)):
-            return math.ceil((high - low + 1) / 2)
-            
-        
+        range = high - low + 1
+        if range % 2 == 1 and (high % 2 == 1 and low % 2 == 1):
+            return math.ceil(range / 2)
+        elif range % 2 == 1 and (high % 2 != 1 and low % 2 != 1):
+            return range // 2
+        elif range % 2 == 0 and ((high % 2 == 1 and low % 2 == 0) or (high % 2 == 0 and low % 2 == 1)):
+            return math.ceil(range / 2)
