627b63414e1a97e98d0cfe3309c673a9bc086b4c
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-14 11:59:20 +0530

    Time: 26 ms (6.01%) | Memory: 60.6 MB (15.15%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        return nums.length != new HashSet<>(Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new))).size();
+    }
+}
