a9b4522fd4a610b974ccd6d0b915a4c123b1f407
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-10-19 15:46:58 +0530

    Time: 42 ms (42.81%) | Memory: 20.4 MB (30.17%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.py
+++ 1006-vowel-spellchecker/vowel-spellchecker.py
@@ -1,7 +1,3 @@
-import re
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
         def devowel(word):
