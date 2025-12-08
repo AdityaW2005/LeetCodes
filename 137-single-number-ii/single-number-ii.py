878b3450d3559bf25b64a9a225f88af4d510048d
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-12-08 15:36:11 +0530

    Time: 36 ms (16.79%) | Memory: 19.1 MB (64.84%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,11 +1,35 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # Brute - Force Solution
+        """
         freq = {}
         for i in nums:
             if i in freq:
                 freq[i] += 1
             else:
                 freq[i] = 1
+
         for i in freq.keys():
             if freq[i] == 1:
                 return i
+        """
+
+        # Shortest Solution
+        """
+        return (3 * sum(set(nums)) - sum(nums)) // 2
+        """
+
+        # Optimal Solution
+        result = 0
+        for i in range(32):  
+            bit_sum = 0
+            for num in nums:
+                if (num >> i) & 1:
+                    bit_sum += 1
+            if bit_sum % 3:
+                result |= (1 << i)
+                
+        if result >= 2**31:
+            result -= 2**32
+
+        return result
