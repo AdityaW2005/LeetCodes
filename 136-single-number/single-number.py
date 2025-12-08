150031e79bfd2ce6f513823f9c6a42267132363d
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2025-12-08 12:01:37 +0530

    Time: 7 ms (32.85%) | Memory: 19.5 MB (76.51%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Create a list of numbers from 0 to 9
-        # Iterate throught the list and compare with the nums list
-        # If count of the particular number is greater than 1 'continue'
-        # Else return the nums element that has 1 frequency
-
-        single_element = 0
+        result = 0
         for i in nums:
-            single_element ^= i
-        return single_element 
-        
-        
+            result ^= i
+        return result
