822daed43ebf9582445c761042b79497285841c7
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-03 23:31:42 +0530

    Time: 7 ms (10.80%) | Memory: 23.5 MB (69.21%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,4 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        # Built - In Method
+        """
         s.reverse()
+        """
+
+        # Two Pointer Approach
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i, j = i + 1, j - 1
