57a16ac9726bdbbc5a2558ec6387901eb79f97c7
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-06 15:37:30 +0530

    Time: 15 ms (11.06%) | Memory: 25.2 MB (5.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered = [c.lower() for c in s if c.isalnum()]
-        return filtered == filtered[::-1]
+        l = list(s)
+        new_l = []
+
+        for i in l:
+            if i.isalpha():
+                new_l.append(i.lower())
+            elif i.isdigit():
+                new_l.append(i)
+
+        print(new_l)
+        
+        i, j = 0, len(new_l) - 1
+
+        while i < j:
+            if new_l[i] == new_l[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
+            
