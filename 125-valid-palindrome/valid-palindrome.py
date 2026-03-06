88920a5ec017c8e561a10ff849a7a2841f4eac0b
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-06 15:39:08 +0530

    Time: 10 ms (38.50%) | Memory: 23.4 MB (15.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,23 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = list(s)
-        new_l = []
+        l = []
 
-        for i in l:
+        for i in s:
             if i.isalpha():
-                new_l.append(i.lower())
+                l.append(i.lower())
             elif i.isdigit():
-                new_l.append(i)
-
-        print(new_l)
+                l.append(i)
+            else:
+                pass
         
-        i, j = 0, len(new_l) - 1
+        i, j = 0, len(l) - 1
 
         while i < j:
-            if new_l[i] == new_l[j]:
+            if l[i] == l[j]:
                 i += 1
                 j -= 1
             else:
                 return False
+
         return True
-            
