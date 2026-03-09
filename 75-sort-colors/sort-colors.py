6b27663ebc953ff18d5fb4cf0f79c2eae8c4eaed
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-09 12:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (22.25%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Brute - Force Approach
+        """
         count_zero = 0
         count_one = 0
         count_two = 0
@@ -23,3 +25,20 @@ class Solution:
 
         for i in range(count_zero + count_one, count_zero + count_one + count_two):
             nums[i] = 2
+        """
+
+        # Optimal Approach (Dutch National Flag)
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
