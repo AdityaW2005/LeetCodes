fa14ad1ac907bce5e05e3db8de730501d8d828fb
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-16 22:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (92.11%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int pivotInteger(int n) {
+        double pivot = Math.sqrt((n * (n + 1) / 2));
+
+        if (pivot % 1 == 0)
+            return (int)pivot;
+
+        return -1;
+    }
+}
