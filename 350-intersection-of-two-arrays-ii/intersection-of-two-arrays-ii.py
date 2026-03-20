1e67b3af4ad891b453f975b11fd2454d0bb3905f
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-20 15:47:49 +0530

    Time: 3 ms (43.32%) | Memory: 19.3 MB (79.22%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Two-Pointer Approach
+        """
         nums1.sort()
         nums2.sort()
         result = []
@@ -16,5 +18,18 @@ class Solution:
                 j += 1
         
         return result
+        """
 
+        # Optimal Approach
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        count = Counter(nums1)
+        result = []
 
+        for num in nums2:
+            if count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+
+        return result
