801e00e80d88660b8943b3e9e6ee9dcd6acbe294
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-03-29 15:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.79%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        if (s1 == s2):
+            return True
+
+        l = list(s2)
+
+        if (l[0] == s1[0]):
+            l[1], l[3] = l[3], l[1]
+            s3 = ''.join(l)
+
+            if (s1 == s3):
+                return True
+            
+        elif (l[1] == s1[1]):
+            l[0], l[2] = l[2], l[0]
+            s4 = ''.join(l)
+
+            if (s1 == s4):
+                return True
+        
+        else:
+            l[1], l[3] = l[3], l[1]
+            l[0], l[2] = l[2], l[0]
+            s5 = ''.join(l)
+
+            if (s1 == s5):
+                return True
+        
+        return False
