aa8a3c84c47cc1d3018a075a7282a6e80a636cab
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-04-02 23:23:42 +0530

    Time: 22 ms (89.36%) | Memory: 77.5 MB (92.85%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -2,31 +2,58 @@ import java.util.*;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        // if (nums == null || nums.length == 0) {
+        //     return 0;
+        // }
+
+        // Set<Integer> set = new HashSet<>();
+        // for (int num : nums) {
+        //     set.add(num);
+        // }
+
+        // int longest = 0;
+
+        // for (int num : set) {
+        //     if (!set.contains(num - 1)) {
+        //         int current = num;
+        //         int length = 1;
+
+        //         while (set.contains(current + 1)) {
+        //             current++;
+        //             length++;
+        //         }
+
+        //         longest = Math.max(longest, length);
+        //     }
+        // }
+
+        // return longest;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int count=1;
+        int longest=1;
+        
+        if(n==0){
             return 0;
         }
+        int lastsmaller=nums[0];
+        
 
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
+        for(int i=1;i<n;i++){
+            if(lastsmaller+1==nums[i]){
+                count++;
+                lastsmaller=nums[i];
 
-        int longest = 0;
-
-        for (int num : set) {
-            if (!set.contains(num - 1)) {
-                int current = num;
-                int length = 1;
-
-                while (set.contains(current + 1)) {
-                    current++;
-                    length++;
-                }
-
-                longest = Math.max(longest, length);
             }
+            else if(nums[i]!=lastsmaller){
+                count=1;
+                lastsmaller=nums[i];
+            }
+            longest=Math.max(longest,count);
         }
-
         return longest;
+
+
+
     }
 }
