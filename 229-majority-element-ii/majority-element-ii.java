83279bbe3cbd11d5956469c5f3b06dd132cf804c
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-04-26 15:22:53 +0530

    Time: 3 ms (75.89%) | Memory: 52.8 MB (22.41%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -26,6 +26,7 @@ class Solution {
 
         List<Integer> res = new ArrayList<>();
         c1 = 0; c2 = 0;
+        
         for (int i = 0; i < nums.length; i++) {
             if (el1 == nums[i]) c1++;
             if (el2 == nums[i]) c2++;
