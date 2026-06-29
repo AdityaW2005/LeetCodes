fad22751b0667af5664f2e6c904a72ca8387936e
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-06-29 11:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (73.88%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,5 @@
 class Solution {
     public int rob(int[] nums) {
-        /*
-        int n = nums.length;
-        if (n == 2) {
-            if (nums[0] > nums[1])
-                return nums[0];
-            else
-                return nums[1];
-        };
-
-        if (n == 3) {
-            if (nums[0] < nums[1] && nums[2] < nums[1] && nums[0] + nums[2] < nums[1])
-                return nums[1];
-            else 
-                return nums[0] + nums[2];
-        }
-
-        int sum = 0;
-        for (int i = 0; i < n; i += 2) {
-            sum += nums[i];
-        }
-
-        return sum;
-        */
-
         int rob = 0;
         int noRob = 0;
 
@@ -33,6 +9,7 @@ class Solution {
             rob = newRob;
             noRob = newNoRob;
         }
+        
         return Math.max(rob, noRob);
     }
 }
