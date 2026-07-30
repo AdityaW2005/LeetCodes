30b4e77886c4927b985380431493851d10259094
W Aditya <116995323+AdityaW2005@users.noreply.github.com>
2026-07-30 12:08:45 +0530

    Time: 3 ms (87.47%) | Memory: 52.5 MB (22.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
+        int m = grid.length; 
         int n = grid[0].length;
         int count = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == '1') {
                     count++;
-                    traverseIsland(grid, i, j); 
+                    dfsIsland(grid, row, col);
                 }
             }
         }
+
         return count;
     }
 
-    public void traverseIsland(char grid[][], int i, int j) {
+    public void dfsIsland(char[][] grid, int row, int col) {
         int m = grid.length;
         int n = grid[0].length;
-
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
+        
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0')
             return;
         
-        grid[i][j] = '0';
-        traverseIsland(grid, i, j + 1); // right
-        traverseIsland(grid, i + 1, j); // down
-        traverseIsland(grid, i, j - 1); // left
-        traverseIsland(grid, i - 1, j); // up
+        grid[row][col] = '0';
+        dfsIsland(grid, row, col + 1);
+        dfsIsland(grid, row - 1, col);
+        dfsIsland(grid, row, col - 1);
+        dfsIsland(grid, row + 1, col);
     }
 }
